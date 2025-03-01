984ed4a7406cb0f2174473db2d39313792353e4c
Facundo Leites <hola@facundoleites.com>
2025-02-28 23:02:46 -0300

    feat: integrate Three.js for 3D model rendering in header and update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-import React, { useCallback, useEffect, useRef } from "react";
-import felicetteLogo from "../images/felicette_logo.png";
+import React, { useEffect, useRef } from "react";
 import { LanguageSelector } from "./LanguageSelector";
 import { getRandomItem } from "../helpers";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from "three";
+const tdmodel = "/Fl4.glb";
+const loader = new GLTFLoader();
 
 const EMOJI_PLACEHOLDER = "🌍";
 
 const defaultHeaderTranslation = {
   apps: "Apps:",
+  projects: "Projects:",
 };
 export const Header = ({
   data,
@@ -78,6 +82,7 @@ export const Header = ({
   }, []);
 
   const mousePosition = useRef({ x: 0, y: 0 });
+  const mousePositionRt = useRef({ x: 0, y: 0 });
   const mouseEventTimeout = useRef<NodeJS.Timeout>();
   const movingRocketTimeout = useRef<NodeJS.Timeout>();
   useEffect(() => {
@@ -109,6 +114,10 @@ export const Header = ({
         "px"
     );
     const listener = (event: MouseEvent) => {
+      mousePositionRt.current = {
+        x: event.clientX,
+        y: event.clientY + document.documentElement.scrollTop,
+      };
       document.documentElement.style.setProperty(
         "--rt-mouse-x",
         `${event.clientX}px`
@@ -169,16 +178,114 @@ export const Header = ({
       document.removeEventListener("mousemove", listener);
     };
   }, []);
+
+  const threeJsContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const containerSize = threeJsContainer.current?.getBoundingClientRect();
+    const aspectRatio =
+      (containerSize?.width ?? 1) / (containerSize?.height ?? 1);
+
+    const defaultCamera = new THREE.PerspectiveCamera(
+      50,
+      aspectRatio,
+      0.1,
+      1000
+    );
+
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setSize(containerSize?.width ?? 10, containerSize?.height ?? 10);
+    renderer.setPixelRatio(window.devicePixelRatio);
+
+    threeJsContainer.current?.appendChild(renderer.domElement);
+
+    defaultCamera.position.z = 5;
+
+    scene.add(defaultCamera);
+
+    loader.load(
+      tdmodel,
+      function (gltf) {
+        const { scene: obj } = gltf;
+
+        obj.updateMatrix();
+        obj.updateWorldMatrix(true, true);
+        scene.add(obj);
+
+        const box = new THREE.Box3().setFromObject(obj);
+        const size = box.getSize(new THREE.Vector3()).length();
+        const center = box.getCenter(new THREE.Vector3());
+
+        obj.position.x -= center.x;
+        obj.position.y -= center.y;
+        obj.position.z -= center.z;
+
+        defaultCamera.near = size / 100;
+        defaultCamera.far = size * 100;
+        defaultCamera.updateProjectionMatrix();
+
+        defaultCamera.position.copy(center);
+        defaultCamera.position.z = 2.3;
+        defaultCamera.lookAt(center);
+
+        const lightsTarget = new THREE.Object3D();
+        lightsTarget.position.copy(center);
+        scene.add(lightsTarget);
+
+        const light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.set(-1, 0, 1);
+        scene.add(light);
+
+        const secondLight = new THREE.DirectionalLight(0x92cdcf, 10);
+
+        secondLight.position.set(2, 0, 0);
+        scene.add(secondLight);
+
+        light.target = lightsTarget;
+        secondLight.target = lightsTarget;
+        renderer.render(scene, defaultCamera);
+
+        let rotationDirection = 1;
+        let lastTime = performance.now();
+
+        const calculateRotationDirection = () => {
+          const containerPos =
+            threeJsContainer.current?.getBoundingClientRect() || {
+              left: 0,
+              width: 0,
+            };
+          const centerOfTheContainer =
+            containerPos.left + containerPos.width / 2;
+          const x = mousePositionRt.current.x - centerOfTheContainer;
+          rotationDirection = x / (window.innerWidth / 2);
+        };
+
+        const animateHorizontalRotation = (currentTime: number) => {
+          const deltaTime = currentTime - lastTime;
+          lastTime = currentTime;
+          obj.rotation.y += 0.00125 * rotationDirection * deltaTime;
+          renderer.render(scene, defaultCamera);
+
+          calculateRotationDirection();
+
+          requestAnimationFrame(animateHorizontalRotation);
+        };
+
+        requestAnimationFrame(animateHorizontalRotation);
+      },
+      undefined,
+      function (error) {
+        console.error(error);
+      }
+    );
+  }, []);
   return (
     <>
       <header className="header">
-        <img
-          className="header__logo"
-          src={felicetteLogo}
-          alt="Felicette logo"
-        />
+        <div ref={threeJsContainer} className="header__three-js-container" />
         <div className="header__main-title__wrapper">
-          <h2 className="header__main-title">{t.apps}</h2>
+          <h2 className="header__main-title">{t.projects}</h2>
         </div>
         <h1 className="header__felicette-title__start" ref={felicetteEl}>
           felicette
