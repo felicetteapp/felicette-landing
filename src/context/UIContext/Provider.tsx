69b6b3aa47a1056e4e4a5a6017d3f482d83f3269
Facundo Leites <leitesfacundo@gmail.com>
2025-06-13 17:37:12 -0300

    feat: update font dependencies to use variable font and add Lenis for smooth scrolling

--- src/context/UIContext/Provider.tsx
+++ src/context/UIContext/Provider.tsx
@@ -1,11 +1,23 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, useCallback, useEffect } from "react";
 import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
 import { UIContext } from "./context";
+import Lenis from "lenis";
 
 export const UIContextProvider = ({ children }: PropsWithChildren) => {
   const { observer, addCallback, observerReady } = useIntersectionObserver(
     () => {}
   );
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <UIContext.Provider
       value={{
